041309e26a1657a38853996b234db330c61b335b
Elmuratovich <makhmudkuzikaev@gmail.com>
2022-04-28 10:31:28 +0600

    SEO optimization, head, title, meta

--- layouts/MainLayout.tsx
+++ layouts/MainLayout.tsx
@@ -6,13 +6,19 @@ import Player from "../components/Player";
 
 interface MainLayoutProps {
     title?: string;
+    descripton?: string;
+    keywords?: string;
 }
 
-const MainLayout: React.FC<MainLayoutProps> = ({ children, title }) => {
+const MainLayout: React.FC<MainLayoutProps> = ({ children, title , descripton, keywords}) => {
     return (
         <>
             <Head>
                 <title>{title || 'Музыкальная плошадка'}</title>
+                <meta name="description" content={`Музыкальная площадка. Здесь каждый может оставить свой трек и стать знаменитым.` + description} />
+                <meta name="robots" content="index, follow" />
+                <meta name="keywords" content={keywords || "Музыка, треки, артисты"} />
+                <meta name="viewport" content="width=device-width, initial-scale=1" />
             </Head>
             <Navbar />
             <Container style={{ margin: '90px 0' }}>
